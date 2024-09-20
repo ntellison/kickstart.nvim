75b7222719536db6d9953ba53ee5b322c676d2e1
Nathan Ellison <ellison.nt@gmail.com>
2024-09-20 14:39:13 -0500

    first commit for kickstart fork. Added a keymap

--- init.lua
+++ init.lua
@@ -160,6 +160,9 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Use jk to do <Esc> in Insert Mode. Quick, easy, dont have to take your fingers of the homerow.
+vim.keymap.set('i', 'jk', '<Esc>')
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
